45e65afcfaeeb3fb2261af209c87b0a4ebc7ea98
colobus-delft <colobus_delft.0t@icloud.com>
2025-01-10 11:33:21 +0300

    Updated datasets

--- datasets/index.php
+++ datasets/index.php
@@ -1,1 +1,2 @@
 Docker Database NLP Event-driven Architecture Performance Serverless
+RabbitMQ Logging Security Message Queue Scalability Cache Invalidation Machine Learning NLP Backend Blockchain Innovation distinguishes between a leader and a follower. Edge Computing Event-driven Architecture
